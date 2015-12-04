de055b2babf6935efc31563bf409c0963f12cd7c
RCCAMER <rcameron@sycamoreleaf.com>
2015-12-04 14:36:40 -0600

    First commit of homewq

---
+++ home.php
@@ -0,0 +1,12 @@
+<! DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Vaping Knowledge</title>
+</head>
+
+<body style='background-image:smoky.jpg' >
+<p>hello world</p>
+</body>
+
+</html>
